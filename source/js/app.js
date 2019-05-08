26208875bada5b5802e8b83a55a5a1cfc19bac9b
k5va <k5va@yandex.ru>
2019-05-08 18:18:28 +0300

    main nav toggle JS added

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,14 @@
-console.log("Hello world 1!");
-console.log("Hello world 2!");
-console.log("Hello world 3!");
-console.log("Hello world 4!");
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
+
+navMain.classList.remove("main-nav--nojs");
+
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
